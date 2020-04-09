a5333ad757c505860040586ef87ccaf43779634c
FacuCuzziol <facucuzziol@gmail.com>
2020-04-09 18:12:58 -0300

    agregado de funcion para imprimir mensaje

--- grupo2/Program.cs
+++ grupo2/Program.cs
@@ -7,6 +7,17 @@ namespace grupo2
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            string mensaje;
+            mensaje = escribir();
+            Console.WriteLine(mensaje);
+        }
+
+        static string escribir()
+        {
+            string mensaje;
+            Console.WriteLine("ingrese el mensaje");
+            mensaje = Console.ReadLine();
+            return mensaje;
         }
     }
 }
